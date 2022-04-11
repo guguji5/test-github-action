a1cbb799b2c49f08567c537366b5ab9805a3ff1d
guguji5 <604821528@qq.com>
2022-04-11 15:55:48 +0800

    7

--- test.js
+++ test.js
@@ -3,15 +3,9 @@ var shell = require('shelljs');
 shell.config.silent = false;
 shell.config.verbose = false;
 
-if (!shell.which('git')) {
-    shell.echo('Sorry, this script requires git');
-    shell.exit(1);
-}
-
 shell.exec('wget https://github.com/n9e/fe-v5/archive/refs/heads/master.zip')
 
-shell.exec('unzip master.zip -d fe-v5')
+shell.exec('unzip master.zip')
 
 shell.exec('ls')
 
-shell.exec('ls fe-v5')
