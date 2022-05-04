75c2f53fb98485b1dbef93fd2ae774f01fd44870
guguji5 <604821528@qq.com>
2022-05-04 16:02:14 +0800

    7

--- matchInDB.js
+++ matchInDB.js
@@ -50,6 +50,7 @@ async function run({holderReduce, liquidStockReduceRatio, tenthLiquidStockRatio,
 }
 
 async function getPriceAndPE(code, { PE, PB, Price }) {
+  console.log('code', code)
   const cmd = `curl 'http://push2.eastmoney.com/api/qt/stock/get?ut=fa5fd1943c7b386f172d6893dbfba10b&invt=2&fltt=2&fields=f43,f57,f58,f169,f170,f46,f44,f51,f168,f47,f164,f163,f116,f60,f45,f52,f50,f48,f167,f117,f71,f161,f49,f530,f135,f136,f137,f138,f139,f141,f142,f144,f145,f147,f148,f140,f143,f146,f149,f55,f62,f162,f92,f173,f104,f105,f84,f85,f183,f184,f185,f186,f187,f188,f189,f190,f191,f192,f107,f111,f86,f177,f78,f110,f260,f261,f262,f263,f264,f267,f268,f250,f251,f252,f253,f254,f255,f256,f257,f258,f266,f269,f270,f271,f273,f274,f275,f127,f199,f128,f193,f196,f194,f195,f197,f80,f280,f281,f282,f284,f285,f286,f287,f292,f293,f181,f294,f295,f279,f288&secid=0.${code}&wbp2u=|0|0|0|web&cb=jQuery112403599250038852819_1651649339070&_=${new Date().getTime()}' \
   -H 'Accept: */*' \
   -H 'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8' \
@@ -61,6 +62,7 @@ async function getPriceAndPE(code, { PE, PB, Price }) {
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36' \
   --compressed \
   --insecure`;
+  console.log('cmd', cmd)
   const output = shell.exec(cmd).stdout;
   var PEReg = /"f162":(.*?),/;
   var PBReg = /"f167":(.*?),/;
