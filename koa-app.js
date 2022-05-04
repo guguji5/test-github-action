8d0315b49acc5e932f0c79434ceb83fc6048707a
guguji5 <604821528@qq.com>
2022-05-04 15:58:28 +0800

    feat: again

--- koa-app.js
+++ koa-app.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 const {run } = require('./matchInDB')
 app.use(async(ctx, next) => {
- const {holderReduce=0, liquidStockReduceRatio=0, tenthLiquidStockRatio=0, numOfholderType=1, eps=0, PE=0, PB=0, Price=0}=ctx.request.query
+ const {holderReduce=0, liquidStockReduceRatio=0, tenthLiquidStockRatio=0, numOfholderType=1, eps=0, PE=100, PB=100, Price=1000}=ctx.request.query
  const codes = await run({holderReduce, liquidStockReduceRatio, tenthLiquidStockRatio, numOfholderType, eps, PE, PB, Price})
   await next();
   ctx.body = codes
