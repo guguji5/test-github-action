5e836fc54c93a0993233e9e10655bd5f311b4db9
guguji5 <604821528@qq.com>
2022-05-05 22:22:12 +0800

    feat: parse request body

--- koa-app.js
+++ koa-app.js
@@ -1,12 +1,15 @@
 const Koa = require('koa');
 const app = new Koa();
+const bodyParser = require('koa-bodyparser');
 const {run } = require('./matchInDB')
 app.use(async(ctx, next) => {
  const {holderReduce=0, liquidStockReduceRatio=0, tenthLiquidStockRatio=0, numOfholderType=1, eps=0, PE=100, PB=100, Price=1000}=ctx.request.query
- if(ctx.request.path !=='/favicon.ico'){
+ if(ctx.request.path ==='/'){
      const codes = await run({holderReduce, liquidStockReduceRatio, tenthLiquidStockRatio, numOfholderType, eps, PE, PB, Price})
      ctx.body = codes
- }else{
+ }else if(ctx.request.path ==='/rebot/send'  ){
+  console.log(ctx.request.body)
+ } else{
     ctx.body = 'damon'
  }
   await next();
