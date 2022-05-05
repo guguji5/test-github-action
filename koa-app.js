68639790cd28cf5643e06d2e02d13528fabd27f2
guguji5 <604821528@qq.com>
2022-05-05 22:30:47 +0800

    21

--- koa-app.js
+++ koa-app.js
@@ -2,7 +2,10 @@ const Koa = require('koa');
 const app = new Koa();
 const bodyParser = require('koa-bodyparser');
 const {run } = require('./matchInDB')
+app.use(bodyParser());
+
 app.use(async(ctx, next) => {
+ console.log(ctx.request.path)
  const {holderReduce=0, liquidStockReduceRatio=0, tenthLiquidStockRatio=0, numOfholderType=1, eps=0, PE=100, PB=100, Price=1000}=ctx.request.query
  if(ctx.request.path ==='/'){
      const codes = await run({holderReduce, liquidStockReduceRatio, tenthLiquidStockRatio, numOfholderType, eps, PE, PB, Price})
