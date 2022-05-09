1dfb11e9ae764e51e4a9d3ac461b031a4b98ebee
guguji5 <604821528@qq.com>
2022-05-09 13:10:33 +0800

    24

--- koa-app.js
+++ koa-app.js
@@ -7,6 +7,7 @@ app.use(bodyParser());
 
 app.use(async(ctx, next) => {
  console.log(ctx.request.path)
+ console.log(ctx.request.body)
  const {holderReduce=0, liquidStockReduceRatio=0, tenthLiquidStockRatio=0, numOfholderType=1, eps=0, PE=100, PB=100, Price=1000}=ctx.request.query
  if(ctx.request.path ==='/'){
      const codes = await run({holderReduce, liquidStockReduceRatio, tenthLiquidStockRatio, numOfholderType, eps, PE, PB, Price})
