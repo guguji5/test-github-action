6eaf5615c095605384b8a7fcedab826d9fe7ac34
guguji5 <604821528@qq.com>
2022-05-11 10:34:11 +0800

    fix word

--- sendDingMessage.js
+++ sendDingMessage.js
@@ -4,7 +4,7 @@ module.exports = { send: async (body)=>{
     const {workflow_run, repository, sender, action} = body
     const cmd = `curl 'https://oapi.dingtalk.com/robot/send?access_token=23929847751c286b5448c40a855c97681a3526c94a631c1a72e1e5b1de0bf8d9' \
     -H 'Content-Type: application/json' \
-    -d '{"msgtype": "text","text": {"content": "普大喜奔\n\n${action ? workflow_run.conclusion +' of ' :''}${repository.name} ${workflow_run.name} ${action} by ${sender.login}"}}'`
+    -d '{"msgtype": "text","text": {"content": "普大喜奔\n\n${action==='completed' ? workflow_run.conclusion +' of ' :''}${repository.name} ${workflow_run.name} ${action} by ${sender.login}"}}'`
 
     shell.exec(cmd);
 } };
