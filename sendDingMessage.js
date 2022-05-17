21505833c206a7daa079761508032e00534e2277
guguji5 <604821528@qq.com>
2022-05-17 15:22:35 +0800

    refactor: update the 文案

--- sendDingMessage.js
+++ sendDingMessage.js
@@ -2,9 +2,20 @@ var shell = require("shelljs");
 
 module.exports = { send: async (body)=>{
     const {workflow_run, repository, sender, action} = body
+    const genTitle = ()=>{
+        if(workflow_run.conclusion ==='success'){
+            return `普大喜奔\n\n`
+        }else if(workflow_run.conclusion ==='failure'){
+            return `Bad News\n\n`
+        }
+        return ''
+    }
+    const genConclusion = ()=>{
+        return action==='completed' ? workflow_run.conclusion +' of ' :''
+    }
     const cmd = `curl 'https://oapi.dingtalk.com/robot/send?access_token=23929847751c286b5448c40a855c97681a3526c94a631c1a72e1e5b1de0bf8d9' \
     -H 'Content-Type: application/json' \
-    -d '{"msgtype": "text","text": {"content": "普大喜奔\n\n${action==='completed' ? workflow_run.conclusion +' of ' :''}${repository.name} ${workflow_run.name} ${action} by ${sender.login}"}}'`
+    -d '{"msgtype": "text","text": {"content": "${genTitle()}${genConclusion()}${repository.name} ${workflow_run.name} ${action} by ${sender.login}"}}'`
 
     shell.exec(cmd);
 } };
