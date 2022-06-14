55c9e812bb6c79931b02d4e6ba4082bd6e19164f
guguji5 <604821528@qq.com>
2022-06-14 15:22:13 +0800

    refactor: notify branch message

--- sendDingMessage.js
+++ sendDingMessage.js
@@ -15,7 +15,7 @@ module.exports = { send: async (body)=>{
     }
     const cmd = `curl 'https://oapi.dingtalk.com/robot/send?access_token=23929847751c286b5448c40a855c97681a3526c94a631c1a72e1e5b1de0bf8d9' \
     -H 'Content-Type: application/json' \
-    -d '{"msgtype": "text","text": {"content": "${genTitle()}${genConclusion()}${repository.name} ${workflow_run.name} ${action} by ${sender.login}"}}'`
+    -d '{"msgtype": "text","text": {"content": "${genTitle()}${genConclusion()}${repository.name} ${workflow_run.name} on ${workflow_run.head_branch} ${action} by ${sender.login}"}}'`
 
     shell.exec(cmd);
 } };
