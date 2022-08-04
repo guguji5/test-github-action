beba66362f8aec693fef629f16e4bab75146ecb1
guguji5 <604821528@qq.com>
2022-08-04 12:11:59 +0800

    refactor: 把通知发到交付群

--- sendDingMessage.js
+++ sendDingMessage.js
@@ -13,9 +13,17 @@ module.exports = { send: async (body)=>{
     const genConclusion = ()=>{
         return action==='completed' ? workflow_run.conclusion +' of ' :''
     }
+    // 三个前端
     const cmd = `curl 'https://oapi.dingtalk.com/robot/send?access_token=23929847751c286b5448c40a855c97681a3526c94a631c1a72e1e5b1de0bf8d9' \
     -H 'Content-Type: application/json' \
     -d '{"msgtype": "text","text": {"content": "${genTitle()}${genConclusion()}${repository.name} ${workflow_run.name} on ${workflow_run.head_branch} ${action} by ${sender.login}"}}'`
 
     shell.exec(cmd);
+    // 交付和部署
+    const cmd1 = `curl 'https://oapi.dingtalk.com/robot/send?access_token=7471e65b572ba8259f0155dc08788e980d1b30f70d66ecabce530f63b68cbcf7' \
+    -H 'Content-Type: application/json' \
+    -d '{"msgtype": "text","text": {"content": "${genTitle()}${genConclusion()}${repository.name} ${workflow_run.name} on ${workflow_run.head_branch} ${action} by ${sender.login}"}}'`
+
+    shell.exec(cmd1);
+    
 } };
