7de12ec5855a9aa313da17c5e99e56b595d0a4e2
guguji5 <604821528@qq.com>
2022-08-24 17:45:26 +0800

    log body

--- koa-app.js
+++ koa-app.js
@@ -1,26 +1,15 @@
-require("appdynamics").profile({
-  controllerHostName: 'tesla2022070316432516.saas.appdynamics.com',
-  controllerPort: 443,
-  
-  // If SSL, be sure to enable the next line
-  controllerSslEnabled: true,
-  accountName: 'tesla2022070316432516',
-  accountAccessKey: 'rsktqzh2oog5',
-  applicationName: 'test',
-  tierName: 'test',
-  nodeName: 'process' // The controller will automatically append the node name with a unique number
- });
-
 const Koa = require('koa');
 const app = new Koa();
 const bodyParser = require('koa-bodyparser');
 const {run } = require('./matchInDB')
 const {send} = require('./sendDingMessage')
+app.use(async(ctx, next) => {
+  console.log(ctx.body)
+  await next()
+})
 app.use(bodyParser());
 
 app.use(async(ctx, next) => {
- console.log(ctx.request.path)
- console.log(ctx.request.body)
  const {holderReduce=0, liquidStockReduceRatio=0, tenthLiquidStockRatio=0, numOfholderType=1, eps=0, PE=100, PB=100, Price=1000}=ctx.request.query
  if(ctx.request.path ==='/'){
      const codes = await run({holderReduce, liquidStockReduceRatio, tenthLiquidStockRatio, numOfholderType, eps, PE, PB, Price})
