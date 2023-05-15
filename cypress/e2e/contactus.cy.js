9d6e96e30f8a438bbe054d71b163d73783d8ec42
guguji5 <604821528@qq.com>
2023-05-15 14:20:26 +0800

    feat: ci 中不做截图和视频的存储

--- cypress/e2e/contactus.cy.js
+++ cypress/e2e/contactus.cy.js
@@ -1,11 +1,10 @@
 describe('联系销售页面', () => {
   it('contactus submit ', () => {
     cy.visit('https://flashcat.cloud/contact/')
-//     cy.get('input[id=name]').type('daily check')
-//     cy.get('input[id=company]').type('contact-us page is available')
-//     cy.get('input[id=phone]').type('00000000')
-//     cy.get('button[type=submit]').click()
-//     cy.get('div#contactResultModal').should('be.visible').should('have.css', 'display', 'block')
-    cy.get('div#contactResultModal').should('be.visible')
+    cy.get('input[id=name]').type('daily check')
+    cy.get('input[id=company]').type('contact-us page is available')
+    cy.get('input[id=phone]').type('00000000')
+    cy.get('button[type=submit]').click()
+    cy.get('div#contactResultModal').should('be.visible').should('have.css', 'display', 'block')
   })
 })
