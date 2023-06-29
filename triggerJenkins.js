d1ed91f3d074c8712c6300376acd5a80fbef332a
早晚会起风 <gracewalk1999@163.com>
2023-06-29 10:43:19 +0800

    Update triggerJenkins.js

--- triggerJenkins.js
+++ triggerJenkins.js
@@ -7,7 +7,8 @@ module.exports = {
 			srm: `curl 'http://10.206.16.3:8090/job/build_fc-frontend_from_github_action/buildWithParameters?token=fc-frontend&zip=${zipVersion}' --user admin:${pwd}`,
 			saas: `curl 'http://10.206.16.3:8090/job/build_fc-saas-web_from_github_action/buildWithParameters?token=fc-frontend&zip=${zipVersion}' --user admin:${pwd}`,
 			'saas-mobile': `curl 'http://10.206.16.3:8090/job/build_fc-saas-mobile_from_github_action/buildWithParameters?token=fc-frontend&zip=${zipVersion}' --user admin:${pwd}`,
-			'saas-cy-pre': 'cd /home/work/fc-foundation-app/ && npx cypress run --record --key e9edfc53-caba-4a53-9ce1-4393918c10e7 --env envFile=pre',
+			'saas-cy-pre': 'cd /home/work/fc-foundation-app/ && npm run cy:run:pre',
+			'saas-cy-prod': 'cd /home/work/fc-foundation-app/ && npm run cy:run:prod',
 		}
 
 		shell.exec(cmdMap[target])
